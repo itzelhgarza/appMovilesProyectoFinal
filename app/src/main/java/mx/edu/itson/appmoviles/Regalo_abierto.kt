129ce0a72eff6d8bbf8b1c6f10327d53c5fcb95f
Garrido <giovanni.garrido228724@potros.itson.edu.mx>
2023-03-21 23:40:15 -0700

    regaloAbierto

---
+++ app/src/main/java/mx/edu/itson/appmoviles/Regalo_abierto.kt
@@ -0,0 +1,11 @@
+package mx.edu.itson.appmoviles
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Regalo_abierto : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_regalo_abierto)
+    }
+}
